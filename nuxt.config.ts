678a80cb68dc944b74dc08812d9fcd786a1b5a16
Luke Mason <Lukemason161@gmail.com>
2026-03-12 17:56:42 +0900

    gh pages fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     baseURL: '/TCGNexus/',
-    buildAssetsDir: '/_nuxt/'
+    buildAssetsDir: '_nuxt/'
   },
   image: {
     provider: 'none'
